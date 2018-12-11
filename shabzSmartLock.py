ace487ecdc6715da05aad13e5b552b16fe9b7cfa
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-12-11 16:10:34 +0100

    Update shabzSmartLock.py

--- shabzSmartLock.py
+++ shabzSmartLock.py
@@ -4,6 +4,7 @@ from sense_hat import SenseHat
 import socket
 import threading
 from threading import Lock, Thread
+import shabzLockDisplay
 
 TCP_IP = "192.168.24.239"
 TCP_PORT = 9576
@@ -16,102 +17,6 @@ messageUnlocked = "The door is unlocked"
 messageJoystickUnlock = "The door is unlocked by joystick"
 messageJoystickLock = "The door is locked by joystick"
 
-g = (0,255,0)
-r = (255,0,0)
-s = (0,0,0)
-b = (153,79,0)
-w = (255,255,255)
-h = (244,164,96)
-d = (0,0,255)
-
-padlock_locked = [
-  s,s,s,s,s,s,s,s,
-  s,s,s,r,r,s,s,s,
-  s,s,r,s,s,r,s,s,
-  s,s,r,s,s,r,s,s,  
-  s,r,r,r,r,r,r,s,
-  s,r,r,r,r,r,r,s,
-  s,r,r,r,r,r,r,s,
-  s,r,r,r,r,r,r,s
-]
-
-padlock_unlocked = [
-  s,s,s,s,s,s,s,s,
-  s,s,s,g,g,s,s,s,
-  s,s,g,s,s,g,s,s,
-  s,s,s,s,s,g,s,s,
-  s,g,g,g,g,g,g,s,
-  s,g,g,g,g,g,g,s,
-  s,g,g,g,g,g,g,s,
-  s,g,g,g,g,g,g,s
-]
-
-tree_locked= [
-  s,s,s,r,r,s,s,s,
-  s,s,s,s,s,s,s,s,
-  s,s,r,r,r,r,s,s,
-  s,s,s,s,s,s,s,s,  
-  s,r,r,r,r,r,r,s,
-  s,s,s,s,s,s,s,s,
-  r,r,r,r,r,r,r,r,
-  s,s,s,b,b,s,s,s
-]
-
-tree_unlocked = [
-  s,s,s,g,g,s,s,s,
-  s,s,s,s,s,s,s,s,
-  s,s,g,g,g,g,s,s,
-  s,s,s,s,s,s,s,s,  
-  s,g,g,g,g,g,g,s,
-  s,s,s,s,s,s,s,s,
-  g,g,g,g,g,g,g,g,
-  s,s,s,b,b,s,s,s
-]
-
-firework_locked= [
-  s,s,s,s,s,s,s,s,
-  r,s,s,r,s,s,r,s,
-  s,r,s,r,s,r,s,s,
-  s,s,r,r,r,s,s,s,  
-  r,r,r,r,r,r,r,s,
-  s,s,r,r,r,s,s,s,
-  s,r,s,r,s,r,s,s,
-  r,s,s,r,s,s,r,s
-]
-
-firework_unlocked = [
-  s,s,s,s,s,s,s,s,
-  g,s,s,g,s,s,g,s,
-  s,g,s,g,s,g,s,s,
-  s,s,g,g,g,s,s,s,  
-  g,g,g,g,g,g,g,s,
-  s,s,g,g,g,s,s,s,
-  s,g,s,g,s,g,s,s,
-  g,s,s,g,s,s,g,s
-]
-
-star_locked= [
-  s,s,r,r,r,r,s,s,
-  s,s,r,r,r,w,r,s,
-  s,s,h,d,h,d,s,s,
-  h,r,h,h,b,b,r,h,  
-  s,s,d,r,r,d,s,s,
-  s,s,d,d,d,d,s,s,
-  s,s,d,d,d,d,s,s,
-  s,s,b,s,s,b,s,s
-]
-
-star_unlocked= [
-  s,s,g,g,g,g,s,s,
-  s,s,g,g,g,w,g,s,
-  s,s,h,d,h,d,s,s,
-  h,g,h,h,b,b,g,h,  
-  s,s,d,g,g,d,s,s,
-  s,s,d,d,d,d,s,s,
-  s,s,d,d,d,d,s,s,
-  s,s,b,s,s,b,s,s
-]
-
 sense = SenseHat()
 GPIO.setmode (GPIO.BOARD)
 GPIO.setup (11, GPIO.OUT)
@@ -123,33 +28,53 @@ lock_display = Lock()
 display = 1
 
 pwm.start(7)
-sense.set_pixels(padlock_locked)
+sense.set_pixels(shabzLockDisplay.padlock_locked)
 
 def set_display():
   
   global display
+  global i
   
   while True:
     
+    lock.acquire()
+    lock.release()
     lock_display.acquire()
     lock_display.release()
+    
     
     for event in sense.stick.get_events():
         if event.direction == "left":
           display = 1
-          sense.set_pixels(padlock_locked)
+          if (i == 0):
+            sense.set_pixels(shabzLockDisplay.padlock_locked)
+          if (i == 1):
+            sense.set_pixels(shabzLockDisplay.padlock_unlocked)
+          
     
         elif event.direction == "right":
           display = 2
-          sense.set_pixels(star_locked)
+          sense.set_pixels(shabzLockDisplay.star_locked)
+          if (i == 0):
+            sense.set_pixels(shabzLockDisplay.star_locked)
+          if (i == 1):
+            sense.set_pixels(shabzLockDisplay.star_unlocked)
     
         elif event.direction == "up":
           display = 3
-          sense.set_pixels(tree_locked)
+          sense.set_pixels(shabzLockDisplay.tree_locked)
+          if (i == 0):
+            sense.set_pixels(shabzLockDisplay.tree_locked)
+          if (i == 1):
+            sense.set_pixels(shabzLockDisplay.tree_unlocked)
           
         elif event.direction == "down":
           display = 4
-          sense.set_pixels(firework_locked)
+          sense.set_pixels(shabzLockDisplay.firework_locked)
+          if (i == 0):
+            sense.set_pixels(shabzLockDisplay.firework_locked)
+          if (i == 1):
+            sense.set_pixels(shabzLockDisplay.firework_unlocked)
 
 def joystick(): 
   
@@ -165,14 +90,14 @@ def joystick():
     
     if (display == 1):
       
-      if (i ==1):
+      if (i == 1):
       
         for event in sense.stick.get_events():
           if event.direction == "middle":
             pwm.ChangeDutyCycle(12)
             sock.send(bytes(messageJoystickUnlock, "UTF-8"))
             sense.show_message(str("HAVE A NICE DAY"), scroll_speed=0.04, text_colour=[0, 0, 255])
-            sense.set_pixels(padlock_unlocked)
+            sense.set_pixels(shabzLockDisplay.padlock_unlocked)
             i = 0
     
       if (i == 0):
@@ -180,7 +105,7 @@ def joystick():
           if event.direction == "middle":
             pwm.ChangeDutyCycle(7)
             sock.send(bytes(messageJoystickLock, "UTF-8"))
-            sense.set_pixels(padlock_locked)
+            sense.set_pixels(shabzLockDisplay.padlock_locked)
             i = 1
             
     if (display == 2):
@@ -192,7 +117,7 @@ def joystick():
             pwm.ChangeDutyCycle(12)
             sock.send(bytes(messageJoystickUnlock, "UTF-8"))
             sense.show_message(str("HAVE A NICE DAY"), scroll_speed=0.04, text_colour=[0, 0, 255])
-            sense.set_pixels(star_unlocked)
+            sense.set_pixels(shabzLockDisplay.star_unlocked)
             i = 0
     
       if (i == 0):
@@ -200,7 +125,7 @@ def joystick():
           if event.direction == "middle":
             pwm.ChangeDutyCycle(7)
             sock.send(bytes(messageJoystickLock, "UTF-8"))
-            sense.set_pixels(star_locked)
+            sense.set_pixels(shabzLockDisplay.star_locked)
             i = 1
     
     if (display == 3):
@@ -212,7 +137,7 @@ def joystick():
             pwm.ChangeDutyCycle(12)
             sock.send(bytes(messageJoystickUnlock, "UTF-8"))
             sense.show_message(str("HAVE A NICE DAY"), scroll_speed=0.04, text_colour=[0, 0, 255])
-            sense.set_pixels(tree_unlocked)
+            sense.set_pixels(shabzLockDisplay.tree_unlocked)
             i = 0
     
       if (i == 0):
@@ -220,7 +145,7 @@ def joystick():
           if event.direction == "middle":
             pwm.ChangeDutyCycle(7)
             sock.send(bytes(messageJoystickLock, "UTF-8"))
-            sense.set_pixels(tree_locked)
+            sense.set_pixels(shabzLockDisplay.tree_locked)
             i = 1
             
     if (display == 4):
@@ -232,7 +157,7 @@ def joystick():
             pwm.ChangeDutyCycle(12)
             sock.send(bytes(messageJoystickUnlock, "UTF-8"))
             sense.show_message(str("HAVE A NICE DAY"), scroll_speed=0.04, text_colour=[0, 0, 255])
-            sense.set_pixels(firework_unlocked)
+            sense.set_pixels(shabzLockDisplay.firework_unlocked)
             i = 0
     
       if (i == 0):
@@ -240,7 +165,7 @@ def joystick():
           if event.direction == "middle":
             pwm.ChangeDutyCycle(7)
             sock.send(bytes(messageJoystickLock, "UTF-8"))
-            sense.set_pixels(firework_locked)
+            sense.set_pixels(shabzLockDisplay.firework_locked)
             i = 1
 
 def recieveMessage():
@@ -263,7 +188,7 @@ def recieveMessage():
       if (message =='l'):
       
         pwm.ChangeDutyCycle(7)
-        sense.set_pixels(padlock_locked)
+        sense.set_pixels(shabzLockDisplay.padlock_locked)
         sock.send(bytes(messageLocked, "UTF-8"))
         i = 1
         lock.acquire()
@@ -275,7 +200,7 @@ def recieveMessage():
         pwm.ChangeDutyCycle(12)        
         sock.send(bytes(messageUnlocked, "UTF-8"))
         sense.show_message(str(name), scroll_speed=0.04, text_colour=[0, 0, 255])
-        sense.set_pixels(padlock_unlocked)
+        sense.set_pixels(shabzLockDisplay.padlock_unlocked)
         i = 0
         lock.acquire()
         lock.release()
@@ -285,7 +210,7 @@ def recieveMessage():
       if (message =='l'):
       
         pwm.ChangeDutyCycle(7)
-        sense.set_pixels(star_locked)
+        sense.set_pixels(shabzLockDisplay.star_locked)
         sock.send(bytes(messageLocked, "UTF-8"))
         i = 1
         lock.acquire()
@@ -296,7 +221,7 @@ def recieveMessage():
         pwm.ChangeDutyCycle(12)        
         sock.send(bytes(messageUnlocked, "UTF-8"))
         sense.show_message(str(name), scroll_speed=0.04, text_colour=[0, 0, 255])
-        sense.set_pixels(star_unlocked)
+        sense.set_pixels(shabzLockDisplay.star_unlocked)
         i = 0
         lock.acquire()
         lock.release()
@@ -306,7 +231,7 @@ def recieveMessage():
       if (message =='l'):
       
         pwm.ChangeDutyCycle(7)
-        sense.set_pixels(tree_locked)
+        sense.set_pixels(shabzLockDisplay.tree_locked)
         sock.send(bytes(messageLocked, "UTF-8"))
         i = 1
         lock.acquire()
@@ -317,7 +242,7 @@ def recieveMessage():
         pwm.ChangeDutyCycle(12)        
         sock.send(bytes(messageUnlocked, "UTF-8"))
         sense.show_message(str(name), scroll_speed=0.04, text_colour=[0, 0, 255])
-        sense.set_pixels(tree_unlocked)
+        sense.set_pixels(shabzLockDisplay.tree_unlocked)
         i = 0
         lock.acquire()
         lock.release()
@@ -327,7 +252,7 @@ def recieveMessage():
       if (message =='l'):
       
         pwm.ChangeDutyCycle(7)
-        sense.set_pixels(firework_locked)
+        sense.set_pixels(shabzLockDisplay.firework_locked)
         sock.send(bytes(messageLocked, "UTF-8"))
         i = 1
         lock.acquire()
@@ -338,7 +263,7 @@ def recieveMessage():
         pwm.ChangeDutyCycle(12)        
         sock.send(bytes(messageUnlocked, "UTF-8"))
         sense.show_message(str(name), scroll_speed=0.04, text_colour=[0, 0, 255])
-        sense.set_pixels(firework_unlocked)
+        sense.set_pixels(shabzLockDisplay.firework_unlocked)
         i = 0
         lock.acquire()
         lock.release()      
